c16b539d93d8bb36bf12a36b93c525a521a8c31b
Elvis <elviskftsang@gmail.com>
2022-06-05 22:55:32 +0800

    edit custom hook

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -14,6 +14,7 @@ export default function useFetch(url) {
                 setStatus('fetched');
             }
             catch (error) {
+                console.error(error);
                 setStatus('error');
             }
         };
